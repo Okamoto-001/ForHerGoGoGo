541b258188f47b85f6e0ccb8ba8e40c92fdef601
isFibonacci <xwcheng497@gmail.com>
2020-01-08 10:01:57 -0600

    update

--- 198.house-robber.cpp
+++ 198.house-robber.cpp
@@ -48,7 +48,16 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        
+        int rob = 0, notRob = 0, n = nums.size();
+
+        for(int i = 0; i < n; i++) {
+            int preRob = rob, preNotRob = notRob;
+
+            rob = preNotRob + nums[i];
+
+            notRob = max(preRob, preNotRob);
+        }
+        return max(rob, notRob);
     }
 };
 // @lc code=end
