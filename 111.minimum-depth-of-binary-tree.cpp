383a0d05037b239ec3e4b4104dce41733c9aca19
isFibonacci <xwcheng497@gmail.com>
2020-01-26 22:05:00 -0600

    update

---
+++ 111.minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=111 lang=cpp
+ *
+ * [111] Minimum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (36.13%)
+ * Likes:    913
+ * Dislikes: 523
+ * Total Accepted:    343.5K
+ * Total Submissions: 949.1K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, find its minimum depth.
+ * 
+ * The minimum depth is the number of nodes along the shortest path from the
+ * root node down to the nearest leaf node.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * return its minimum depth = 2.
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root) return 0;
+
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+
+        if (!left || !right) return left+right+1;
+
+        return min(left, right) + 1;
+    }
+};
+// @lc code=end
+
--- 279.perfect-squares.cpp
+++ 279.perfect-squares.cpp
@@ -35,18 +35,40 @@
 class Solution {
 public:
     int numSquares(int n) {
-        vector<int> dp(n+1, __INT_MAX__);
+
+        vector<int> dp(n+1, INT_MAX);
+
         dp[0] = 0;
 
         for (int i = 0; i <= n; i++) {
-            for (int j = 1; i + j*j <= n; j++) {
-                dp[i + j*j] = min(dp[i+j*j], dp[i]+1);
+            for (int j = 1; 1+j*j <= n; j++) {
+                dp[i+j*j] = min(dp[i+j*j], dp[i]+1);
             }
         }
 
         return dp.back();
 
+        // vector<int> dp(n+1, __INT_MAX__);
+        // dp[0] = 0;
+
+        // for (int i = 0; i <= n; i++) {
+        //     for (int j = 1; i + j*j <= n; j++) {
+        //         dp[i + j*j] = min(dp[i+j*j], dp[i]+1);
+        //     }
+        // }
+
+        // return dp.back();
+
     }
 };
+
+// n = 5
+// i   j   i+j*j   dp[i]+1   dp[i+j*j]
+// 0   1   1        1           1
+//     2   4       0+1 = 1      1
+// 1   1   2       2           2
+//     2   5        1+1 = 2    2    
+// 3   3+1  4      1+1          2
+// 4   1    5       2           2  
 // @lc code=end
 
